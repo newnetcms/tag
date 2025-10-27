baacec39745b12d8223d4e467a64ffd652a32a1f
Newnet Dev <dev@newnet.vn>
2025-10-27 10:16:30 +0700

    fix tags url

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,19 @@
 
 use Newnet\Tag\Http\Controllers\Web\TagController;
 
-Route::prefix('tags')->group(function () {
-    Route::get('{slug}-{id}', [TagController::class, 'detail'])
-        ->where([
-            // ✅ Cho phép chữ cái (mọi ngôn ngữ), số và dấu gạch ngang
-            'slug' => '[\pL\pN\-\_]+',
-            'id'   => '[0-9]+',
-        ])
-        ->name('tag.web.tag.detail');
-});
+
+Route::prefix(LaravelLocalization::setLocale())
+    ->middleware([
+        'localizationRedirect',
+    ])
+    ->group(function () {
+        Route::prefix('tags')->group(function () {
+            Route::get('{slug}-{id}', [TagController::class, 'detail'])
+                ->where([
+                    // ✅ Cho phép chữ cái (mọi ngôn ngữ), số và dấu gạch ngang
+                    'slug' => '[\pL\pN\-\_]+',
+                    'id' => '[0-9]+',
+                ])
+                ->name('tag.web.tag.detail');
+        });
+    });
