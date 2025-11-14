e0cb74877e3d945b9713242dd086f7a3e6dc8797
Newnet Dev <dev@newnet.vn>
2025-11-14 09:44:49 +0700

    fix match tag url

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,14 @@ Route::prefix(LaravelLocalization::setLocale())
         'localizationRedirect',
     ])
     ->group(function () {
+        Route::any('tags/{any}', [TagController::class, 'index'])
+            ->where('any', '.*');
+        
         Route::prefix('tags')->group(function () {
             Route::get('{slug}-{id}', [TagController::class, 'detail'])
                 ->where([
                     // ✅ Cho phép chữ cái (mọi ngôn ngữ), số và dấu gạch ngang
-                    // 'slug' => '[\pL\pN\-\_]+',
+                    'slug' => '[\pL\pN\-\_]+',
                     'id' => '[0-9]+',
                 ])
                 ->name('tag.web.tag.detail');
--- src/Http/Controllers/Web/TagController.php
+++ src/Http/Controllers/Web/TagController.php
@@ -7,6 +7,18 @@ use Newnet\Tag\Models\Tag;
 
 class TagController extends Controller
 {
+    public function index($any)
+    {
+        $id = intval(substr($any, strrpos($any, '-') + 1));
+        $slug = substr($any, 0, strrpos($any, '-'));
+
+        if ($id <= 0) {
+            abort(404);
+        }
+
+        return $this->detail($slug, $id);
+    }
+
     public function detail($slug, $id)
     {
         $tag = Tag::findorFail($id);
